2b053a48f32fd7fe6d46228b3d1303fb25029e5b
navya3416 <49711518+navya3416@users.noreply.github.com>
2019-06-17 15:42:18 +0530

    redirect the output of docker build

--- wrap.py
+++ wrap.py
@@ -7,6 +7,6 @@ a = 1
 while(a==1):
     mac = input("enter macaddress: ")
     FNULL = open(os.devnull, 'w')
-    retcode = subprocess.call(["docker","build", "-t", "navya","."])
+    retcode = subprocess.call(["docker","build", "-t", "navya","."], stdout=FNULL, stderr=subprocess.STDOUT)
     subprocess.call(["docker","run","navya:latest",apikey,mac])
     a = int(input("do you want check another mac if yes enter 1 otherwise enter 0: "))
