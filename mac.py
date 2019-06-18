e937eda485866e809db3c018a0b89d3fa7712dd0
navya3416 <49711518+navya3416@users.noreply.github.com>
2019-06-18 11:07:27 +0530

    removing too mant try and except blocks

--- mac.py
+++ mac.py
@@ -4,18 +4,15 @@ import sys
 try:
   apikey=sys.argv[1]
   MacAddress=sys.argv[2]
-except:
-  print ("Enter apikey and macaddress as arguments")
-url = 'https://api.macaddress.io/v1'
-params = dict(
-    output='json'
-)
-try:                                                                                                                                                                                        
+  url = 'https://api.macaddress.io/v1'
+  params = dict(
+      output='json'
+  )                                                                                                                                                                                        
   params['search']=MacAddress
   params['apikey']=apikey
   resp = requests.get(url=url, params=params)                                                                                                                                               
   info = resp.json()    
-except:
-  print("problem occurred while connecting to api")
+except Exception as e:
+  print(e)
 else:
   print("MAC Address " + MacAddress+ " is associated with "+info['vendorDetails']['companyName'] )
