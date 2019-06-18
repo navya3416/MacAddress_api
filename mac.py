3958e0d3163970f4db6b550bf07c45ee1ba1aa2c
navya3416 <49711518+navya3416@users.noreply.github.com>
2019-06-18 11:44:03 +0530

    adding comments to script

--- mac.py
+++ mac.py
@@ -1,6 +1,8 @@
+# importing modules
 import requests
 import json
 import sys
+# giving the arguments for user input and giving the url and connecting to api with user given arguments
 try:
   apikey=sys.argv[1]
   MacAddress=sys.argv[2]
@@ -11,8 +13,10 @@ try:
   params['search']=MacAddress
   params['apikey']=apikey
   resp = requests.get(url=url, params=params)                                                                                                                                               
-  info = resp.json()    
+  info = resp.json()
+# exception handling     
 except Exception as e:
   print(e)
+# if not exception printing the output
 else:
   print("MAC Address " + MacAddress+ " is associated with "+info['vendorDetails']['companyName'] )
