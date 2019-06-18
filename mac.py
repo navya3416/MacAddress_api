c70e62c02fe1f39b0e05532819eff81e436335d1
navya3416 <49711518+navya3416@users.noreply.github.com>
2019-06-18 10:49:26 +0530

    changes made for excepting handling connecting to api

--- mac.py
+++ mac.py
@@ -1,32 +1,21 @@
 import requests
 import json
 import sys
-
 try:
- apikey=sys.argv[1]
-except IndexError:
- print("Apikey not found")
-
-try:
- MacAddress=sys.argv[2]
-except IndexError:
- print("Address not found")
-
+  apikey=sys.argv[1]
+  MacAddress=sys.argv[2]
+except:
+  print ("Enter apikey and macaddress as arguments")
 url = 'https://api.macaddress.io/v1'
 params = dict(
     output='json'
 )
-
 try:                                                                                                                                                                                        
   params['search']=MacAddress
   params['apikey']=apikey
-except NameError:
- print("Something went wrong")
-
-resp = requests.get(url=url, params=params)                                                                                                                                               
-info = resp.json()    
-
-try:
-  print("MAC Address " + MacAddress+ " is associated with "+info['vendorDetails']['companyName'] )
+  resp = requests.get(url=url, params=params)                                                                                                                                               
+  info = resp.json()    
 except:
-  print ("Data not found")
+  print("problem occurred while connecting to api")
+else:
+  print("MAC Address " + MacAddress+ " is associated with "+info['vendorDetails']['companyName'] )
